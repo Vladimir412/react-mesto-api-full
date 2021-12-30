c88e70feb66fa138109bdc120d010604316eaea6
unknown <greddyturbo@mail.ru>
2021-12-30 23:41:50 +0300

    add cors response

--- backend/app.js
+++ backend/app.js
@@ -67,9 +67,11 @@ app.use(requestLogger);
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
   }
 
   const { method } = req;
